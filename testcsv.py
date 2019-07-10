7f125096c9badddf8250cee5970b3e9b883d6d43
EliasRubin <33438216+EliasRubin@users.noreply.github.com>
2019-07-10 05:03:15 -0700

    Update testcsv.py

--- testcsv.py
+++ testcsv.py
@@ -3,14 +3,8 @@ import csv
 import sys
 import argparse
 
-with open(sys.argv[1]) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+
+with open('sys.argv[1]', newline='') as File:  
+    reader = csv.reader(File)
+    for row in reader:
+        print(row)
