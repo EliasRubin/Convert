552032bb01e6fe921e1faa4fe5229fc31e221711
EliasRubin <33438216+EliasRubin@users.noreply.github.com>
2019-07-10 04:46:49 -0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -13,7 +13,25 @@ try:
         with open(sys.argv[1], "r") as inFile:
             content = inFile.readlines()
             name,ext = os.path.splitext(inFile.name)
-            new = toCsv(content)
+            
+            data = False
+            header = ""
+            newContent = []
+            for line in content:
+                if not data:
+                    if "@attribute" in line:
+                        attri = line.split()
+                        columnName = attri[attri.index("@attribute")+1]
+                        header = header + columnName + ","
+                    elif "@data" in line:
+                        data = True
+                        header = header[:-1]
+                        header += '\n'
+                        newContent.append(header)
+                    else:
+                        newContent.append(line)
+                        new = newContent
+
             print(new)
         
          
@@ -29,22 +47,4 @@ except FileNotFoundError:
             
             
             
-            
-def toCsv(content):
-    data = False
-    header = ""
-    newContent = []
-    for line in content:
-        if not data:
-            if "@attribute" in line:
-                attri = line.split()
-                columnName = attri[attri.index("@attribute")+1]
-                header = header + columnName + ","
-            elif "@data" in line:
-                data = True
-                header = header[:-1]
-                header += '\n'
-                newContent.append(header)
-        else:
-            newContent.append(line)
-    return newContent
+         
