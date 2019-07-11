f41090919ee0623804520b6eca6912e194a10789
EliasRubin <33438216+EliasRubin@users.noreply.github.com>
2019-07-11 09:13:33 -0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -57,7 +57,10 @@ try:
             i = 0
             while i < len(headers):
                 dataJson.append("")
+                print(headers[i])
                 i += 1
+            
+            
             
             i = 1
             while i < len(newCsv):
